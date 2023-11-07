26d02de405d80fa83fce27c4e51e19fd590ed41a
Your Name <you@example.com>
2023-11-07 00:35:08 -0800

    Topo ring

--- topos/ring.py
+++ topos/ring.py
@@ -24,6 +24,25 @@ class TreeTopo( Topo ):
 
         # Add links
         # > self.addLink([HOST1], [HOST2])
+        with open('ring.in', 'r') as f:
+            lines = f.readlines()
+        
+        num_hosts,num_switches,num_links = map(int,lines[0].split())
+
+        #Add hosts
+        for i in range(1,num_hosts+1):
+            self.addHost('h%d' % i)
+
+
+        #Add switches
+        for i in range(1,num_switches+1):
+            sconfig = {'dpid': "%016x" % i}
+            self.addSwitch('s%d' % i, **sconfig)
+        
+        #Add links
+        for line in lines[1:]:
+            h1,h2 = line.rstrip('\n').split(',')
+            self.addLink(h1,h2)
         
                     
 topos = { 'sdnip' : ( lambda: TreeTopo() ) }
